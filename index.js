687d2c113cfffb63c46670520c32d1afaa0dd10f
gabriele.fuoco <gabriele.fuoco99@gmail.com>
2026-03-16 22:38:31 +0100

    Fix: add missing 'fs' import in index.js for branding assets

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const path = require('path');
+const fs = require('fs');
 const { createAxiosInstance } = require('./src/utils/httpClient');
 const rateLimit = require('express-rate-limit');
 
