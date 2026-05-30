9f389070564b3b118e544ba773a417b2457d3850
gabriele.fuoco <gabriele.fuoco99@gmail.com>
2026-05-30 20:03:22 +0200

    fix(ui): fix material symbols not loading correctly

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -13,6 +13,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="it" className="dark">
+      <head>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" />
+      </head>
       <body className="antialiased font-display bg-background-light dark:bg-background-dark text-zinc-900 dark:text-zinc-100 min-h-screen">
         <div className="relative flex h-auto min-h-screen w-full flex-col overflow-x-hidden">
           <div className="layout-container flex h-full grow flex-col">
