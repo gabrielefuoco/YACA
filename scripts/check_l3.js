d82894d3701252843ee6acab5d96d4b722b7b22f
gabriele.fuoco <gabriele.fuoco99@gmail.com>
2026-07-23 16:27:53 +0200

    chore(scripts): purge sandbox scripts and consolidate testing tools

--- scripts/check_l3.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const graph = JSON.parse(fs.readFileSync('./src/data/hierarchical_graph.json', 'utf8'));
-
-for (const [id, data] of Object.entries(graph.L3 || {})) {
-    console.log(`[${id}] Top Keywords:`);
-    console.log(data.top_keywords.slice(0, 15).join(', '));
-    console.log('---');
-}
